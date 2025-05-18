9eaf7d902ad5c344b02f27e998639a00376270a5
wedadee <wedadee03@gmail.com>
2025-05-18 06:04:50 +0300

    Update urls.py

--- Mustashrik/urls.py
+++ Mustashrik/urls.py
@@ -4,6 +4,8 @@ Definition of urls for Mustasharik.
 
 from datetime import datetime
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
    path("", include("app.urls")),
@@ -11,3 +13,5 @@ urlpatterns = [
     path('contract/', include('contract_analyzer_app.urls')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
