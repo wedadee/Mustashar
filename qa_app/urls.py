f0a218fba3bbf18df3cdb3ed4c5d0fe88fd34831
wedadee <wedadee03@gmail.com>
2025-05-18 06:05:38 +0300

    Update urls.py

--- qa_app/urls.py
+++ qa_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 app_name = "qa_app"
 
 urlpatterns = [
     path("", views.qa_view, name="qa_view"),
     path("api/", views.qa_api, name="qa_api"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
