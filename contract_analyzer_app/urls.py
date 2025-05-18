9d659e3f11e3c7f2edccdc51ac2f2ba601ccca78
wedadee <wedadee03@gmail.com>
2025-05-18 06:06:16 +0300

    Update urls.py

--- contract_analyzer_app/urls.py
+++ contract_analyzer_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.upload_pdf, name='contract-view'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
